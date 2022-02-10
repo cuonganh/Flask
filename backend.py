295c9ac89ae5c64301f47fbf32e53751ae003df9
CuongTV <46061394+cuonganh@users.noreply.github.com>
2022-02-10 18:12:44 +0700

    sessions
    - store data temporarily on the webserver
    - creating permanent sessions and deleting old session data

--- backend.py
+++ backend.py
@@ -1,6 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "https://github.com/cuonganh"
+app.permanent_session_lifetime = timedelta(minutes=30)
 
 @app.route("/")
 def home():
@@ -9,14 +12,28 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        # session.permanent = True
         user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    # clear session when logout
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
