b167226f32be1790b1753bbecafa069cbf12d835
CuongTV <46061394+cuonganh@users.noreply.github.com>
2022-02-10 22:28:45 +0700

    flash message
    Flashing messages is very easy and a good way to make your website more responsive

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -15,9 +15,11 @@ def login():
         # session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+        flash("Login successful")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash("Already logged in")
             return redirect(url_for("user"))
         return render_template("login.html")
 
@@ -25,13 +27,15 @@ def login():
 def user():
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+        return render_template("user.html", user=user)
     else:
+        flash("You are not logged in")
         return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
-    # clear session when logout
+    # clear session when logout 
+    flash("You are logged out your account", "info")
     session.pop("user", None)
     return redirect(url_for("login"))
 
