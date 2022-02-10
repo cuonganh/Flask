148fae3afb4039eaaff74078b25724b58205be6e
CuongTV <46061394+cuonganh@users.noreply.github.com>
2022-02-10 14:57:39 +0700

    build backend with Flask

---
+++ backend.py
@@ -0,0 +1,16 @@
+from contextlib import redirect_stderr
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    return render_template("new.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
