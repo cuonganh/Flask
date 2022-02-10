e49023ca94cdd0d28b0cc39bf2982bfc43dcb477
CuongTV <46061394+cuonganh@users.noreply.github.com>
2022-02-10 23:49:59 +0700

    Using SQLAlchemy Database
    - setup a database with flask using SqlAlchemy
    - SQLAlchemy allows you to execute SQL queries directly with python code
    - can create models that represent database information and easily modify and delete existing entries

--- backend.py
+++ backend.py
@@ -1,9 +1,25 @@
+from unicodedata import name
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "https://github.com/cuonganh"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=30)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+    
 
 @app.route("/")
 def home():
@@ -23,11 +39,19 @@ def login():
             return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return render_template("user.html", user=user)
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            flash("Your email was saved")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", email=email)
     else:
         flash("You are not logged in")
         return redirect(url_for("login"))
@@ -37,7 +61,9 @@ def logout():
     # clear session when logout 
     flash("You are logged out your account", "info")
     session.pop("user", None)
+    session.pop("email", None)
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
