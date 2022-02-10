bd9674555711febcb5566854a41b1b463e988ba3
CuongTV <46061394+cuonganh@users.noreply.github.com>
2022-02-10 17:13:54 +0700

    HTTP request methods Post and Get

--- backend.py
+++ backend.py
@@ -1,5 +1,4 @@
-from contextlib import redirect_stderr
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -7,10 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/test")
-def test():
-    return render_template("new.html")
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form("nm")
+        return redirect(url_for("user", usr = "user"))
+    else:
+        return render_template("login.html")
 
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
