65b325a145cff94fb24b8e32d18c20fa6e0f3921
CuongTV <46061394+cuonganh@users.noreply.github.com>
2022-02-12 00:58:59 +0700

    using static with css and image

--- backend.py
+++ backend.py
@@ -21,8 +21,9 @@ class users(db.Model):
     
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/view")
 def view():
